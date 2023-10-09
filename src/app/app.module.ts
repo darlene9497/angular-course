b08b2f75f31b0b44ab73ce8bdba23db9459180c2
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-10-09 14:42:26 +0300

    :sparkles: components and core directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CourseCardComponent } from './course-card/course-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseCardComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
