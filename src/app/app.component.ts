4d4bab3195ca4acd27019e9d695b77d6dce61527
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-10-10 11:44:11 +0300

    :construction: completed section 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,17 @@ export class AppComponent {
 
   courses = COURSES;
 
+  /**Built-in pipes */
+  // title = COURSES[0].description;
+
+  // startDate = new Date(2000, 0, 1)
+
+  // price = 9.99
+
+  // rate = 0.89
+
+  // course = COURSES[0]
+
   onCourseSelected(course:Course){
     console.log("App component - button clicked...", course)
   }
